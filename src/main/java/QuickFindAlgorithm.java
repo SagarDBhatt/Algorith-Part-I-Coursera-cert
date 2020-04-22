0e43df08208b9274ae3f6291e0abc9c6e35a45ad
Sagar Bhatt <49220046+Sagar1087@users.noreply.github.com>
2020-04-21 23:35:41 -0600

    Create QuickFindAlgorithm.java

---
+++ src/main/java/QuickFindAlgorithm.java
@@ -0,0 +1,9 @@
+/**
+ * This class contains QuickFindAlgorithm and its illustration to check union and connection operation.
+ */
+
+public class QuickFindAlgorithm {
+    public static void main(String[] args) {
+
+    }
+}
