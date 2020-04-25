4a662405a0c6b525a6de0918f3c60a23878f682b
Sagar Bhatt <49220046+Sagar1087@users.noreply.github.com>
2020-04-24 23:13:06 -0600

    Update QuickUnion.java

--- src/main/java/QuickUnion.java
+++ src/main/java/QuickUnion.java
@@ -1,12 +1,21 @@
 public class QuickUnion {
 
+    /**
+     * Set id of each object to itself. (N array access)
+     */
     private int[] id;
 
     public static void main(String[] args) {
 
     }
 
+    /**
+     * Creating int array with given size
+     * @param n : provide the size of array
+     */
     public QuickUnion(int n){
-        
+        id = new int[n];
     }
+
+    
 }
