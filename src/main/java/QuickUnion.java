3e71e05a3f6587708ac56631da6097b331f78c9d
Sagar Bhatt <49220046+Sagar1087@users.noreply.github.com>
2020-04-24 23:26:10 -0600

    Final solution QuickUnion.java

--- src/main/java/QuickUnion.java
+++ src/main/java/QuickUnion.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class QuickUnion {
 
     /**
@@ -6,7 +8,15 @@ public class QuickUnion {
     private int[] id;
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the object size = ");
+        int n = sc.nextInt();
 
+        QuickUnion obj = new QuickUnion(n);
+        obj.union(2,4);
+        System.out.println(obj.isConnected(2,4));
+        System.out.println(obj.root(2) + " -- " + obj.root(3));
+        System.out.println(obj.isConnected(2,3));
     }
 
     /**
@@ -15,7 +25,27 @@ public class QuickUnion {
      */
     public QuickUnion(int n){
         id = new int[n];
+        for(int i = 0; i < n; i++)
+            id[i] = i;
     }
 
-    
+    public int root(int n){
+        while(n != id[n]) {
+            n = id[n];
+        }
+        return n;
+    }
+
+    public void union(int p, int q){
+        int i = root(p);
+        int j = root(q);
+        id[i] = j;
+    }
+
+    public boolean isConnected(int p, int q){
+        if (root(p) == root(q))
+            return true;
+        else
+            return false;
+    }
 }
